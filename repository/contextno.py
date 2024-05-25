c797252ecf89a64152daa289d926ba3e0d4cefcc
PolinaLack <polina.lack@gmail.com>
2024-05-25 23:36:06 +0300

    add try-exc and with-as

--- repository/contextno.py
+++ repository/contextno.py
@@ -34,48 +34,55 @@ class Tip(WordRank):
 word = "колбаса"
  
 async def start_game(http_session):
-    resp: ClientResponse = await http_session.get(
-        url="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot"
-        )
-    new_cont_sess = ContextnoSessionIn(**await resp.json())
-    game_sess = ContextnoSession(challenge_id=new_cont_sess.id)
+    try:
+        resp: ClientResponse = await http_session.get(
+            url="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot"
+            )
+        new_cont_sess = ContextnoSessionIn(**await resp.json())
+        game_sess = ContextnoSession(challenge_id=new_cont_sess.id)
+    except Exception as e:
+            print(f"Error starting session: {e}")
     return game_sess
 
 
 async def get_word_rank(challenge_id, http_session, word):
-    url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_score?"
-           "challenge_id={}&user_id=contextno_bot&word={}&challenge_type=random")
-    resp: ClientResponse = await http_session.get(url.format(challenge_id, word))
-    word_rank = WordRank(**await resp.json())
-    print(word_rank)
+    try:
+        url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_score?"
+            "challenge_id={}&user_id=contextno_bot&word={}&challenge_type=random")
+        resp: ClientResponse = await http_session.get(url.format(challenge_id, word))
+        word_rank = WordRank(**await resp.json())
+        print(word_rank)
+    except Exception as e:
+            print(f"Error get word rank: {e}")
     return word_rank
 
 
 async def get_tip(challenge_id, http_session):
-    url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_tip?"
-           "challenge_id={}&user_id=contextno_bot&challenge_type=random")
-    resp: ClientResponse = await http_session.get(url.format(challenge_id))
-    tip = Tip(**await resp.json())
-    print(tip)
+    try:
+        url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_tip?"
+            "challenge_id={}&user_id=contextno_bot&challenge_type=random")
+        resp: ClientResponse = await http_session.get(url.format(challenge_id))
+        tip = Tip(**await resp.json())
+        print(tip)
+    except Exception as e:
+        print(f"Error get tip: {e}")
     return tip
 
 
 async def main():
-    http_session = ClientSession()
-    task1: Task[ContextnoSession] = create_task(start_game(http_session=http_session))
-    game_sess: ContextnoSession = await task1
-    
-    if game_sess.challenge_id is not None:
-        task2: Task[WordRank] = create_task(get_word_rank(
-            http_session=http_session, challenge_id=game_sess.challenge_id, word=word))
-        await task2
+    with ClientSession() as http_session:
+        task1: Task[ContextnoSession] = create_task(start_game(http_session=http_session))
+        game_sess: ContextnoSession = await task1
         
-        task3: Task[Tip] = create_task(get_tip(
-            http_session=http_session, challenge_id=game_sess.challenge_id))
-        await task3
+        if game_sess.challenge_id is not None:
+            task2: Task[WordRank] = create_task(get_word_rank(
+                http_session=http_session, challenge_id=game_sess.challenge_id, word=word))
+            await task2
+            
+            task3: Task[Tip] = create_task(get_tip(
+                http_session=http_session, challenge_id=game_sess.challenge_id))
+            await task3
         
-    await http_session.close()
-
 
 if __name__ == "__main__":
     run(main=main())
