f63de768edb1e8713dfa7384418770a383e3ade6
PolinaLack <polina.lack@gmail.com>
2024-05-25 15:25:05 +0300

    Revert "add .http_session in ContextnoSession"
    This reverts commit 62de414c7b09ade13c9ea9f516497cb26f9156c0.

--- repository/contextno.py
+++ repository/contextno.py
@@ -10,8 +10,7 @@ from pydantic import BaseModel
 
 
 class ContextnoSession(BaseModel):
-    challenge_id: str = None  # type: ignore
-    http_session: ClientSession
+    id: str
     chat_id: str = None  # type: ignore
 
 class ContextnoSessionIn(ContextnoSession):
@@ -40,45 +39,68 @@ async def start_session():
         url="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot"
         )
     new_cont_sess = ContextnoSessionIn(**await resp.json())
-    game_session = ContextnoSession(challenge_id=new_cont_sess.id, http_session=my_http_session)
-    return game_session
+    challenge_id: str = new_cont_sess.id
+    return challenge_id, my_http_session
 
 
-async def get_word_rank(game_session, word):
+async def get_word_rank(challenge_id, my_http_session, word):
     url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_score?"
            "challenge_id={}&user_id=contextno_bot&word={}&challenge_type=random")
-    resp: ClientResponse = await game_session.http_session.get(url.format(game_session.challenge_id, word))
+    resp: ClientResponse = await my_http_session.get(url.format(challenge_id, word))
     word_rank = WordRank(**await resp.json())
     print(word_rank)
     return word_rank
 
 
-async def get_tip(game_session):
+async def get_tip(challenge_id, my_http_session):
     url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_tip?"
            "challenge_id={}&user_id=contextno_bot&challenge_type=random")
-    resp: ClientResponse = await game_session.http_session.get(url.format(game_session.challenge_id))
+    resp: ClientResponse = await my_http_session.get(url.format(challenge_id))
     tip = Tip(**await resp.json())
     print(tip)
     return tip
 
 
 async def main():
-    task1: Task[ContextnoSession] = create_task(start_session())
-    game_session: ContextnoSession = await task1
+    task1: Task[tuple[str, ClientSession]] = create_task(start_session())
+    challenge_id, my_http_session = await task1
     
-    if game_session.challenge_id is not None:
-                
+    if challenge_id is not None:
         task2: Task[WordRank] = create_task(get_word_rank(
-            game_session=game_session, word=word))
+            my_http_session=my_http_session, challenge_id=challenge_id, word=word))
         await task2
         
         task3: Task[Tip] = create_task(get_tip(
-            game_session))
+            my_http_session=my_http_session, challenge_id=challenge_id))
         await task3
         
     
-    await game_session.http_session.close()
+    await my_http_session.close()
 
 
 if __name__ == "__main__":
     run(main=main())
+
+# bot = Bot(token='your_bot_token')
+# dp = Dispatcher(bot)
+# @dp.message.handler(commands=["start"])
+# async def start_game():
+    
+    
+#     response = web.get(
+#         path="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot",
+#         handler=web.json_response()
+#         )
+#     # response = "smth"
+#     print(response)
+    
+#     # await message.reply("получили сессию")
+    
+    
+    
+# if __name__ == "__main__":
+#     asyncio.run(start_game())
+    
+	# С помощью метода executor.start_polling опрашиваем
+     # Dispatcher: ожидаем команду /start
+	# executor.start_polling(dp)
