7cc6e93cb564cb52a01cc7428dd631c88518eda8
PolinaLack <polina.lack@gmail.com>
2024-05-25 15:54:53 +0300

    add http_session as arg, separate ContextnoSession and ContextnoSessionIn

--- repository/contextno.py
+++ repository/contextno.py
@@ -10,10 +10,10 @@ from pydantic import BaseModel
 
 
 class ContextnoSession(BaseModel):
-    id: str
+    challenge_id: str
     chat_id: str = None  # type: ignore
 
-class ContextnoSessionIn(ContextnoSession):
+class ContextnoSessionIn(BaseModel):
     id: str
     name: str
 
@@ -33,74 +33,50 @@ class Tip(WordRank):
 
 word = "колбаса"
  
-async def start_session():
-    my_http_session = ClientSession()
-    resp: ClientResponse = await my_http_session.get(
+async def start_game(http_session):
+    resp: ClientResponse = await http_session.get(
         url="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot"
         )
     new_cont_sess = ContextnoSessionIn(**await resp.json())
-    challenge_id: str = new_cont_sess.id
-    return challenge_id, my_http_session
+    game_sess = ContextnoSession(challenge_id=new_cont_sess.id)
+    return game_sess
 
 
-async def get_word_rank(challenge_id, my_http_session, word):
+async def get_word_rank(challenge_id, http_session, word):
     url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_score?"
            "challenge_id={}&user_id=contextno_bot&word={}&challenge_type=random")
-    resp: ClientResponse = await my_http_session.get(url.format(challenge_id, word))
+    resp: ClientResponse = await http_session.get(url.format(challenge_id, word))
     word_rank = WordRank(**await resp.json())
     print(word_rank)
     return word_rank
 
 
-async def get_tip(challenge_id, my_http_session):
+async def get_tip(challenge_id, http_session):
     url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_tip?"
            "challenge_id={}&user_id=contextno_bot&challenge_type=random")
-    resp: ClientResponse = await my_http_session.get(url.format(challenge_id))
+    resp: ClientResponse = await http_session.get(url.format(challenge_id))
     tip = Tip(**await resp.json())
     print(tip)
     return tip
 
 
 async def main():
-    task1: Task[tuple[str, ClientSession]] = create_task(start_session())
-    challenge_id, my_http_session = await task1
+    http_session = ClientSession()
+    task1: Task[ContextnoSession] = create_task(start_game(http_session=http_session))
+    game_sess: ContextnoSession = await task1
     
-    if challenge_id is not None:
+    if game_sess.challenge_id is not None:
         task2: Task[WordRank] = create_task(get_word_rank(
-            my_http_session=my_http_session, challenge_id=challenge_id, word=word))
+            http_session=http_session, challenge_id=game_sess.challenge_id, word=word))
         await task2
         
         task3: Task[Tip] = create_task(get_tip(
-            my_http_session=my_http_session, challenge_id=challenge_id))
+            http_session=http_session, challenge_id=game_sess.challenge_id))
         await task3
         
-    
-    await my_http_session.close()
+    await http_session.close()
 
 
 if __name__ == "__main__":
     run(main=main())
 
-# bot = Bot(token='your_bot_token')
-# dp = Dispatcher(bot)
-# @dp.message.handler(commands=["start"])
-# async def start_game():
-    
-    
-#     response = web.get(
-#         path="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot",
-#         handler=web.json_response()
-#         )
-#     # response = "smth"
-#     print(response)
-    
-#     # await message.reply("получили сессию")
-    
-    
-    
-# if __name__ == "__main__":
-#     asyncio.run(start_game())
-    
-	# С помощью метода executor.start_polling опрашиваем
-     # Dispatcher: ожидаем команду /start
-	# executor.start_polling(dp)
