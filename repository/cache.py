6cdbb3a3b7c4dd77be9a53c0c22b143b77a59d77
AleksGin <alexanderginin@icloud.com>
2024-05-27 20:22:40 +0300

    some minor changes

--- repository/cache.py
+++ repository/cache.py
@@ -1,21 +1,23 @@
-import aioredis
+from aioredis import Redis
 
+
+WORD_RANK_KEY = "word_rank:{}"
 
 class CacheRepository:
-    def __init__(self, redis_url: str):
-        self.redis = aioredis.from_url(redis_url, decode_responses=True)
-        self.word_rank_key = "word_rank:{}"
+    def __init__(self, redis: Redis):
+        self.redis = redis
         
     async def set_challenge_id_by_chat_id(self, chat_id: str, challenge_id: str):
-         await self.redis.set(f"challenge_id:{chat_id}", challenge_id)
+         await self.redis.set(f"{chat_id}", challenge_id)
          
     async def get_challenge_id_by_chat_id(self, chat_id: str):
-        await self.redis.get(f"challenge_id:{chat_id}")
+        await self.redis.get(f"{chat_id}")
         
     async def set_word_rank(self, chat_id: str, word: str, rank: int):
-        key = self.word_rank_key.format(chat_id)
+        key = WORD_RANK_KEY.format(chat_id)
         await self.redis.zadd(key, {word: rank}, nx=True)
 
     async def get_five_closests_words_by_chat_id(self, chat_id: str):
-        key = self.word_rank_key.format(chat_id)
+        key = WORD_RANK_KEY.format(chat_id)
         return await self.redis.zrange(key, 0, 4, withscores=True)
+    
