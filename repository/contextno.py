b6fe335f7b975cf98f9cd84d141d29291e5972e4
PolinaLack <polina.lack@gmail.com>
2024-05-29 10:01:32 +0300

    edits made

--- repository/contextno.py
+++ repository/contextno.py
@@ -5,28 +5,36 @@ from models.contextno import Tip, WordRank
 
 class ContextnoRepo:
     def __init__(self, http_session: ClientSession) -> None:
-        self.http_session: ClientSession = http_session
+        self.http_session = http_session
+        self.url = "https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai"
         
     
-    async def get_challenge_id(self, chat_id) -> int:
-        resp = await self.http_session.get(
-            url="https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_random_challenge?user_id=contextno_bot"
-            )
-        sess_data = await resp.json()
-        return int(sess_data['id'])
+    async def get_challenge_id(self, chat_id: str) -> str:
+        url= self.url + "/get_random_challenge"
+        params = {'user_id': 'contextno_bot'}
+        async with self.http_session.get(url=url, params=params) as resp:
+            sess_data = await resp.json()
+            return sess_data['id']
 
 
-    async def get_word_rank(self, challenge_id, word) -> WordRank:
-        url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_score?"
-            "challenge_id={}&user_id=contextno_bot&word={}&challenge_type=random")
-        resp = await self.http_session.get(url.format(challenge_id, word))
-        word_rank = WordRank(**await resp.json())
-        return word_rank
+    async def get_word_rank(self, challenge_id: str, word: str) -> WordRank:
+        params = {
+                'challenge_id': challenge_id,
+                'word': word,
+                'user_id': 'contextno_bot',
+                'challenge_type': 'random'}
+        url= self.url + "/get_score"
+        async with self.http_session.get(url, params=params) as resp:
+            word_rank = WordRank(**await resp.json())
+            return word_rank
 
 
-    async def get_tip(self, challenge_id) -> Tip:
-        url = ("https://xn--80aqu.xn--e1ajbkccewgd.xn--p1ai/get_tip?"
-            "challenge_id={}&user_id=contextno_bot&challenge_type=random")
-        resp = await self.http_session.get(url.format(challenge_id))
-        tip = Tip(**await resp.json())
-        return tip
+    async def get_tip(self, challenge_id: str) -> Tip:
+        params = {
+                'challenge_id': challenge_id,
+                'user_id': 'contextno_bot',
+                'challenge_type': 'random'}
+        url= self.url + "/get_tip"
+        async with self.http_session.get(url, params=params) as resp:
+            tip = Tip(**await resp.json())
+            return tip
